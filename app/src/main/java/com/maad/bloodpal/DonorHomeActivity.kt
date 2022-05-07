01926f2b8d0817b2afd771ba252b4811e50e4c11
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-07 02:47:05 +0200

    designzz

---
+++ app/src/main/java/com/maad/bloodpal/DonorHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.maad.bloodpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonorHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donor_home)
+    }
+}
---
+++ app/src/main/java/com/maad/bloodpal/PatientHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.maad.bloodpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_home)
+    }
+}
