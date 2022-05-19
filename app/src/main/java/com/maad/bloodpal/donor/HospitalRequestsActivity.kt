090b54f32ce8f5f2f6e9cb564035a6d41987dbc8
AhmedMaad <atef.ahmed1341@gmail.com>
2022-05-19 09:14:16 +0200

    Blood Request List Item

--- app/src/main/java/com/maad/bloodpal/donor/HospitalRequestsActivity.kt
+++ app/src/main/java/com/maad/bloodpal/donor/HospitalRequestsActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import com.maad.bloodpal.databinding.ActivityHospitalRequestsBinding
 
 class HospitalRequestsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHospitalRequestsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
+
 }
